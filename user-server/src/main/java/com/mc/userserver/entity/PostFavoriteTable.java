fceb011a5637619f042787b3192e2c9f74f35405
肖建明 <xiao_lo01@163.com>
2023-06-09 13:19:09 +0800

    帖子点赞收藏分享实体类

--- user-server/src/main/java/com/mc/userserver/entity/PostFavoriteTable.java
+++ user-server/src/main/java/com/mc/userserver/entity/PostFavoriteTable.java
@@ -3,40 +3,28 @@ package com.mc.userserver.entity;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.google.common.collect.PeekingIterator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.PipedReader;
 import java.time.LocalDateTime;
 
 /**
  * @作者：XMC
  * @邮箱：1309478453@qq.com
- * @创建时间： 2023-06-09 11:33
+ * @创建时间： 2023-06-09 13:09
  * @类说明：填写类说明
  * @修改记录：
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PostDetailTable {
+public class PostFavoriteTable {
     @TableId
-    private String postDtailId;
-    private String postTypeCode;
-    private String userId;
-    private String postTitle;
-    private String postContent;
-    private String postImage;
-
-    private String postPublishPosition;
-    private String postLikeCount;
-    private String postCommentCount;
-    private String postFavoriteCount;
-    private String postShareCount;
-    private String postWeight;
-
+    private String favoriteId;
+    private String favoritePostId;
+    private String befavoriteUserId;
+    private String favoriteStatus;
 
     @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
---
+++ user-server/src/main/java/com/mc/userserver/mapper/PostFavoriteMapper.java
@@ -0,0 +1,17 @@
+package com.mc.userserver.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mc.userserver.entity.PostFavoriteTable;
+import com.mc.userserver.entity.PostLikeTable;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @作者：XMC
+ * @邮箱：1309478453@qq.com
+ * @创建时间： 2023-06-09 13:13
+ * @类说明：填写类说明
+ * @修改记录：
+ */
+@Mapper
+public interface PostFavoriteMapper extends BaseMapper<PostFavoriteTable> {
+}
